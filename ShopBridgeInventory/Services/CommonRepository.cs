55996a5552950e1e58999241538728f6f5fed564
dhruti9011 <84681041+dhruti9011@users.noreply.github.com>
2021-05-23 23:58:35 +0530

    thinkbridge test
    Thinkbridge test

---
+++ ShopBridgeInventory/Services/CommonRepository.cs
@@ -0,0 +1,38 @@
+﻿using ShopBridgeInventory.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBridgeInventory.Services
+{
+    public class CommonRepository<T> : ICommonRepository<T> where T : class
+    {
+        private readonly InventoryMgmtContext _context;
+
+        public CommonRepository(InventoryMgmtContext context)
+        {
+            _context = context;
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public async Task<T> SaveAsync(T entity)
+        {
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
